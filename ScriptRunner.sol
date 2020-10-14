f3368600b1f0965ee6c36cd7768c77f729f630e2
Steve Corya <steve@stevecorya.com>
2020-10-13 21:51:06 -0400

    clarify struct variable names

--- ScriptRunner.sol
+++ ScriptRunner.sol
@@ -5,16 +5,16 @@ pragma experimental ABIEncoderV2;
 
 contract ScriptRunner {
     struct Call {
-        address target;
-        uint256 value;
-        bytes callData;
+        address _target;
+        uint256 _value;
+        bytes _callData;
     }
 
     function runScript(Call[] memory calls) public payable returns (bytes[] memory returnData) {
 
         returnData = new bytes[](calls.length);
         for(uint8 i = 0; i < calls.length; i++) {
-            (bool success, bytes memory data) = calls[i].target.call{value:calls[i].value}(calls[i].callData);
+            (bool success, bytes memory data) = calls[i]._target.call.value(calls[i]._value)(calls[i]._callData);
             require(success, string(data));
             returnData[i] = data;
         }
